e030d372bca36d61ed217f603e019899db164666
Roman Dubrovsky <r.dubrovsky@datarockets.com>
2024-11-24 13:38:37 +0100

    Setup RSpec

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,27 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation, except: %w[ar_internal_metadata])
+    Rails.application.load_seed
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:suite) do
+    DatabaseCleaner.clean_with(:truncation, except: %w[ar_internal_metadata])
+    Rails.application.load_seed
+  end
+end
